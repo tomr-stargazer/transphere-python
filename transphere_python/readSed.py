5fd099382af929d1a4c90b34e82ea45342850849
Tom Rice <t.rice90@gmail.com>
2017-11-12 12:59:39 +0100

    extend the readSed function to allow passing optional separator args to the str.split() fn.

--- transphere_python/readSed.py
+++ transphere_python/readSed.py
@@ -1,11 +1,11 @@
-def readSed(filename):
+def readSed(filename, sep=None):
     f = open(filename, 'r')
     inpd=f.readlines()
 
     sed={'wave': [], 'flux': [], 'instrument': []}
 
     for ii in range(0, len(inpd)):
-        aa, bb, cc=inpd[ii].strip().split()
+        aa, bb, cc=inpd[ii].strip().split(sep=sep)
         sed['wave'].append(float(aa))
         sed['flux'].append(float(bb))
         sed['instrument'].append(cc)
